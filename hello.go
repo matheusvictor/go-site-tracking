c319cd533b8e2748fc248ea84a42a74b4ea51714
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-21 08:24:52 -0300

    Commit inicial

---
+++ hello.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func menu() {
+	fmt.Println("1 - Iniciar monitoramento")
+	fmt.Println("2 - Exibir logs")
+	fmt.Println("0 - Sair do programa")
+}
+
+func main() {
+	var nome string = "Matheus"
+	sobrenome := "Santana" // forma "abreviada" de declarar uma variável com inferência de tipo
+
+	var versao float32 = 1.1
+	fmt.Println("Olá,", nome, sobrenome)
+	fmt.Println("Este programa está na versão", versao)
+	menu()
+
+	var opcao int
+	fmt.Scanf("%d", &opcao) // o valor será atributo à variável a partir de seu endereço
+	// fmt.Println("Endereço da variável opcao: ", &opcao)
+	fmt.Println("A opção escolhida foi", opcao)
+
+}
