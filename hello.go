25c07a2786622b6d14b052ec0295e6e4562b05f9
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-21 13:55:32 -0300

    feat: Add requisições sucessivas com pausas

--- hello.go
+++ hello.go
@@ -4,14 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"time"
 )
+
+const quantidadeMonitoramento = 2
+const delay = 60
 
 func exibirIntroducao() {
 	var nome string = "Matheus"
 	sobrenome := "Santana" // forma "abreviada" de declarar uma variável com inferência de tipo
 
 	var versao float32 = 1.1
-	fmt.Println("Olá,", nome, sobrenome)
+	fmt.Println("Olá,", nome, sobrenome, "!")
 	fmt.Println("Este programa está na versão", versao)
 }
 
@@ -27,6 +31,16 @@ func exibirMenu() {
 	fmt.Println("0 - Sair do programa")
 }
 
+func testarSite(site string) {
+	response, _ := http.Get(site)
+
+	if response.StatusCode == 200 {
+		fmt.Println("Site", site, "foi acessado com sucesso!")
+	} else {
+		fmt.Println("Site", site, "está com problemas! Status Code:", response.StatusCode)
+	}
+}
+
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
 
@@ -36,20 +50,16 @@ func iniciarMonitoramento() {
 		"https://www.google.com.br",
 		"https://random-status-code.herokuapp.com/"}
 
-	//  index, site := range sites
-	for index := 0; index < len(sites); index++ {
-		testarSite(sites[index])
+	for index := 1; index <= quantidadeMonitoramento; index++ {
+		fmt.Println("Executando", index, "ª rotina de monitoramento...")
+		//  index, site := range sites
+		for index := 0; index < len(sites); index++ {
+			testarSite(sites[index])
+		}
+		fmt.Println("Aguardando para testar novamente...")
+		time.Sleep(delay * time.Second)
 	}
-}
 
-func testarSite(site string) {
-	response, _ := http.Get(site)
-
-	if response.StatusCode == 200 {
-		fmt.Println("Site", site, "foi acessado com sucesso!")
-	} else {
-		fmt.Println("Site", site, "está com problemas! Status Code:", response.StatusCode)
-	}
 }
 
 func main() {
