c919c388f751f9ce1b1069510a37a9c639b6863b
Matheus Victor <matheusvictor.salles@gmail.com>
2023-09-27 08:53:29 -0300

    feat: Monitoramento de sites e registro de logs
    - Corrige leitura/escrita de arquivo
    - Add arquivos de exemplo

--- hello.go
+++ hello.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -20,11 +21,32 @@ func main() {
 		exibirMenu()
 		opcao := lerOpcao()
 		switch opcao {
-		case 1:
+		case 1: // Iniciar monitoramento
 			iniciarMonitoramento()
-		case 2:
+		case 2: // Cadastrar novo site para monitoramento
+			var arquivoSites *os.File = nil
+
+			arquivoSites = abrirArquivo("sites")
+			if arquivoSites == nil {
+				fmt.Println("Arquivo de sites não encontrando! Criando arquivo...")
+				criarArquivo("sites")
+				fmt.Println("Arquivo criado com sucesso!")
+			}
+
+			registrarSite("loerm")
+		case 3: // Exibir logs
+			var arquivoLogs *os.File = nil
+			arquivoLogs = abrirArquivo("logs")
+
+			if arquivoLogs == nil {
+				fmt.Println("Arquivo de logs não encontrando! Criando arquivo...")
+				criarArquivo("logs")
+				fmt.Println("Arquivo criado com sucesso!")
+			}
+
 			fmt.Println("Exibindo logs...")
-		case 0:
+
+		case 0: // Sair do programa
 			fmt.Println("Saindo do programa")
 			os.Exit(0)
 		default:
@@ -56,25 +78,16 @@ func exibirMenu() {
 	fmt.Println("0 - Sair do programa")
 }
 
-func testarSite(site string) {
-	response, erro := http.Get(site)
-
-	if erro != nil {
-		fmt.Println("Ocorreu um erro:", erro)
-	}
-
-	if response.StatusCode == 200 {
-		fmt.Println("Site", site, "foi acessado com sucesso!")
-	} else {
-		fmt.Println("Site", site, "está com problemas! Status Code:", response.StatusCode)
-	}
-}
-
 func iniciarMonitoramento() {
-	fmt.Println("Monitorando...")
+	fmt.Println("Monitorando sites...")
 
 	// Slices são como arrays dinâmicos
 	sites := lerSitesDoArquivo()
+
+	if len(sites) == 0 {
+		fmt.Println("Não há sites para serem monitorados!")
+		return
+	}
 
 	for index := 1; index <= quantidadeMonitoramento; index++ {
 		fmt.Println("Executando", index, "ª rotina de monitoramento...")
@@ -87,41 +100,88 @@ func iniciarMonitoramento() {
 	}
 }
 
-func abrirArquivo() *os.File {
-
-	// arquivo, erro := ioutil.ReadFile("sites.txt")
-	arquivo, erro := os.Open("sites.txt")
+func testarSite(site string) {
+	response, erro := http.Get(site)
 
 	if erro != nil {
 		fmt.Println("Ocorreu um erro:", erro)
 	}
 
-	return arquivo
-}
-
-func criarArquivo() {
-	os.Create("teste.txt")
+	if response.StatusCode == 200 {
+		fmt.Println("Site", site, "foi acessado com sucesso!")
+		registrarLog(site, true)
+	} else {
+		fmt.Println("Site", site, "está com problemas! Status Code:", response.StatusCode)
+		registrarLog(site, false)
+	}
 }
 
 func lerSitesDoArquivo() []string {
 
 	var sites []string
-	arquivo := abrirArquivo()
+	arquivo := abrirArquivo("sites")
 
-	leitor := bufio.NewReader(arquivo)
-	for {
-		linha, erro := leitor.ReadString('\n') // aspas simples para definir o byte
-		linha = strings.TrimSpace(linha)
-		fmt.Println(linha)
+	if arquivo != nil {
+		leitor := bufio.NewReader(arquivo)
+		for {
+			linha, erro := leitor.ReadString('\n') // aspas simples para definir o byte
+			linha = strings.TrimSpace(linha)
 
-		sites = append(sites, linha)
+			// se a primeira linha for uma string vazia, não há sites para monitorar
+			if len(linha) == 0 {
+				break
+			}
 
-		if erro == io.EOF { // identifica final do arquivo
-			break
+			sites = append(sites, linha)
+
+			if erro == io.EOF { // identifica final do arquivo
+				break
+			}
 		}
+		arquivo.Close()
+	}
+	return sites
+}
+
+func abrirArquivo(nomeArquivo string) *os.File {
+	arquivo, erro :=
+		os.OpenFile(fmt.Sprintf("%s.%s", nomeArquivo, "txt"), os.O_RDWR|os.O_APPEND, 0666)
+
+	if erro != nil {
+		fmt.Println("Ocorreu um erro:", erro.Error())
+		return nil
+	}
+	return arquivo
+}
+
+func criarArquivo(nomeArquivo string) {
+	novoArquivo, erro := os.OpenFile(
+		fmt.Sprintf("%s.%s", nomeArquivo, "txt"),
+		os.O_CREATE|os.O_RDWR|os.O_APPEND, 0666)
+
+	if erro != nil {
+		fmt.Println("Ocorreu um erro:", erro)
+		return
+	}
+	fmt.Println("Arquivo", novoArquivo, "criado com sucesso!")
+}
+
+func registrarSite(site string) {
+	arquivoSites := abrirArquivo("sites")
+	arquivoSites.WriteString(site + "\n")
+	arquivoSites.Close()
+}
+
+func registrarLog(site string, status bool) {
+	var arquivoLogs *os.File = nil
+	nomeArquivo := "logs"
+
+	arquivoLogs = abrirArquivo(nomeArquivo)
+	if arquivoLogs == nil {
+		criarArquivo(nomeArquivo)
 	}
 
-	arquivo.Close()
-
-	return sites
+	arquivoLogs = abrirArquivo(nomeArquivo)
+	arquivoLogs.WriteString("Site: " + site + " [Online: " + strconv.FormatBool(status) + "] \n")
+	arquivoLogs.Close()
 }
